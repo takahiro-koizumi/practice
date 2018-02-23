434807a1de0254800cb1bbf86a3a962c93d8bdb0
takahiro-koizumi <t.k.10w20.a@gmail.com>
2018-02-23 16:35:27 +0900

    jsを使ったモーダルウィンドウ

---
+++ first/styles/js/main.js
@@ -0,0 +1,40 @@
+// クリックしたら、背景が暗くなりモーダルウィンドウが出てくる。
+(function() {
+  'use strict';
+
+// 関係ある全てのidを個々に取得し、変数に代入
+  var open = document.getElementById('open');
+  var close = document.getElementById('close');
+  var mask = document.getElementById('mask');
+  var modal = document.getElementById('modal');
+
+// id="open"をクリックした時の処理
+  open.addEventListener('click', function() {
+    modal.className = '';
+    mask.className = '';
+  });
+
+// id="close"をクリックした時の処理
+  close.addEventListener('click', function() {
+    modal.className = 'hidden';
+    mask.className = 'hidden';
+  });
+
+// id="mask"をクリックした時の処理
+  mask.addEventListener('click', function() {
+    // modal.className = 'hidden';
+    // mask.className = 'hidden';
+    // 上記と同じ記述でも同じ動作を行うが、こっちの方がスマート
+    // closeのclick functionを使うよー的な
+    close.click();
+  });
+  modal.addEventListener('click', function() {
+    // modal.className = 'hidden';
+    // mask.className = 'hidden';
+    // 上記と同じ記述でも同じ動作を行うが、こっちの方がスマート
+    // closeのclick functionを使うよー的な
+    close.click();
+  });
+
+
+})();
